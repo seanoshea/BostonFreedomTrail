cce3512bf18b2a438b950cdb98b6ee7bf9c17885
seanoshea <oshea.ie@gmail.com>
2016-01-17 21:29:58 -0500

    Correcting the tests

--- BostonFreedomTrailTests/Utils/TrailParserSpec.swift
+++ BostonFreedomTrailTests/Utils/TrailParserSpec.swift
@@ -48,10 +48,10 @@ class TrailParserTest: QuickSpec {
             
             context("Testing Parsing of Placemarks") {
                 
-                it("should parse out sixteen placemarks from the trail xml file") {
+                it("should parse out seventeen placemarks from the trail xml file") {
                     let trailParser = TrailParser()
                     let trail = trailParser.parseTrail()
-                    expect(trail.placemarks.count).to(equal(16))
+                    expect(trail.placemarks.count).to(equal(17))
                 }
             }
         }
