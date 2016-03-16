bb98b0a3eb40b9e45c6549f958c67c49611c47e4
seanoshea <oshea.ie@gmail.com>
2016-03-15 21:48:19 -0400

    Per issue #90 - A way to look up LookAts by place mark identifier

--- BostonFreedomTrail/Model/VirtualTourModel.swift
+++ BostonFreedomTrail/Model/VirtualTourModel.swift
@@ -43,12 +43,12 @@ enum VirtualTourStopStopDuration : Double {
     case CameraRepositionAnimation = 0.5
     case DefaultDelay = 1.0
     case DelayForCameraRepositioning = 2.0
-    case DelayForLookAt = 10.0
+    case DelayForLookAt = 5.0
 }
 
 class VirtualTourModel : NSObject {
     var tour:[CLLocation] = []
-    var lookAts = [Int:LookAt]()
+    var lookAts = [Int:Int]()
     var placemarkDemarkations = [Int:Int]()
     var currentTourLocation:Int = 0
     var currentTourState:VirtualTourState = VirtualTourState.BeforeStart
@@ -59,7 +59,7 @@ class VirtualTourModel : NSObject {
             for (locationIndex, location) in placemark.coordinates.enumerate() {
                 if locationIndex == placemark.coordinates.count - 1 {
                     if placemark.lookAt != nil {
-                        self.lookAts[index] = placemark.lookAt
+                        self.lookAts[index] = placemarkIndex
                     }
                 }
                 index = index + 1
@@ -81,7 +81,9 @@ class VirtualTourModel : NSObject {
     
     func lookAtForCurrentLocation() -> LookAt? {
         guard self.currentTourLocation > 0 else { return nil}
-        return self.lookAts[self.currentTourLocation]
+        let placemarkIndex = self.lookAts[self.currentTourLocation]
+        let placemark = Trail.instance.placemarks[placemarkIndex!]
+        return placemark.lookAt
     }
     
     func placemarkForCurrentLookAt() -> Placemark {
@@ -140,6 +142,21 @@ class VirtualTourModel : NSObject {
             delay = VirtualTourStopStopDuration.DelayForLookAt.rawValue
         }
         return dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
+    }
+    
+    func lookAtForPlacemarkIndex(placemarkIndex:Int) -> LookAt? {
+        var placemark:Placemark?
+        for (_, value) in self.lookAts {
+            if value == placemarkIndex {
+                placemark = Trail.instance.placemarks[value]
+                break
+            }
+        }
+        if let foundPlacemark = placemark {
+            return foundPlacemark.lookAt!
+        } else {
+            return nil
+        }
     }
 
 // MARK: Calculating Camera Directions
